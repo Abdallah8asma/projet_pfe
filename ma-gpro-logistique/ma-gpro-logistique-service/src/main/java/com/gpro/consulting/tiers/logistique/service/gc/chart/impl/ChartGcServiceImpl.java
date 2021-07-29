4364513d59bf8f83928dc53238e9043c70a29ab3
Samer <samer.gproconsulting@gmail.com>
2021-07-29 15:17:43 +0100

    #logistique #reglementChart

--- ma-gpro-logistique/ma-gpro-logistique-service/src/main/java/com/gpro/consulting/tiers/logistique/service/gc/chart/impl/ChartGcServiceImpl.java
+++ ma-gpro-logistique/ma-gpro-logistique-service/src/main/java/com/gpro/consulting/tiers/logistique/service/gc/chart/impl/ChartGcServiceImpl.java
@@ -16,7 +16,7 @@ import com.gpro.consulting.tiers.logistique.coordination.gc.bonlivraison.value.R
 import com.gpro.consulting.tiers.logistique.coordination.gc.bonlivraison.value.RechercheMulticritereDetLivraisonValue;
 import com.gpro.consulting.tiers.logistique.coordination.gc.chart.value.ResultBestElementValue;
 import com.gpro.consulting.tiers.logistique.coordination.gc.reglement.value.RechercheMulticritereReglementValue;
-import com.gpro.consulting.tiers.logistique.coordination.gc.reglement.value.RegelementChartValue;
+import com.gpro.consulting.tiers.logistique.coordination.gc.reglement.value.ReglementChartValue;
 import com.gpro.consulting.tiers.logistique.coordination.gc.reglement.value.RegelementReportElementRecapValue;
 import com.gpro.consulting.tiers.logistique.coordination.gc.reglement.value.ResultatRechecheReglementElementValue;
 import com.gpro.consulting.tiers.logistique.coordination.gc.report.vente.facture.value.BLReportElementRecapValue;
@@ -188,7 +188,7 @@ public class ChartGcServiceImpl implements IChartGcService {
 	}
 
 	@Override
-	public List<RegelementChartValue> getReglementChart(RechercheMulticritereReglementValue request) {
+	public List<ReglementChartValue> getReglementChart(RechercheMulticritereReglementValue request) {
 		// TODO Auto-generated method stub
 		return chartGcDomaine.getReglementChart(request);
 	}
