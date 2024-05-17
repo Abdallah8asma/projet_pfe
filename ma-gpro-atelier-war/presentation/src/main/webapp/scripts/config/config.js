4773681955782b99e4285e18205f0032e7b7a8ef
Asma  Abdallah <asmaabdallah518@gmail.com>
2024-05-17 14:23:27 +0000

    Update config.js

--- ma-gpro-atelier-war/presentation/src/main/webapp/scripts/config/config.js
+++ ma-gpro-atelier-war/presentation/src/main/webapp/scripts/config/config.js
@@ -2,5 +2,5 @@
  * IpSTIT: 192.168.1.150
  */
 angular.module('config', [ "ngResource" ])
-    .constant('UrlCommun', "http://54.173.244.248:8080/mt-gpro-commun-rest")
-    .constant('UrlAtelier', "http://54.173.244.248:8080/ma-gpro-logistique-rest");
+    .constant('UrlCommun', "http://localhost:8080/mt-gpro-commun-rest")
+    .constant('UrlAtelier', "http://localhost:8080/ma-gpro-logistique-rest");
