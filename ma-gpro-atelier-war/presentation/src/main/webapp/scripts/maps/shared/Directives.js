8ee7bf4d56097bfa09d06af4de14709cfd358cba
Rabeb Nouira <rabebnouira30@gmail.com>
2024-05-31 09:43:24 +0000

    Update Directives.js

--- ma-gpro-atelier-war/presentation/src/main/webapp/scripts/maps/shared/Directives.js
+++ ma-gpro-atelier-war/presentation/src/main/webapp/scripts/maps/shared/Directives.js
@@ -466,7 +466,7 @@ angular
 						controller : function($scope, $element, $http,$resource, baseUrl) {
 							// declaration variables
 							var baseUrlGs = "http://localhost:8080/ma-gpro-gs-rest";
-							var baseUrl = "http://localhost:8080/mt-gpro-commun-rest";
+							var baseUrl = "http://localhost:8080/mt-gpro-commun-rest-3.5.0.0-SNAPSHOT";
 							$scope.matrix = [ [], [], [], [], [] ];
 							$scope.elementValueRow = [];
 							$scope.elementNGgridShow = true;
