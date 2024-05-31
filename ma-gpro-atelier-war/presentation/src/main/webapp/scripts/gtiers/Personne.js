e7600247d1294ee3423eda35258d4f91d47b508a
Rabeb Nouira <rabebnouira30@gmail.com>
2024-05-31 09:42:22 +0000

    Update Personne.js

--- ma-gpro-atelier-war/presentation/src/main/webapp/scripts/gtiers/Personne.js
+++ ma-gpro-atelier-war/presentation/src/main/webapp/scripts/gtiers/Personne.js
@@ -2,7 +2,7 @@
 
 angular
   .module('theme.personne', ["ngResource"])
- .constant("baseUrl", "http://localhost:8080/mt-gpro-commun-rest")
+ .constant("baseUrl", "http://localhost:8080/mt-gpro-commun-rest-3.5.0.0-SNAPSHOT")
   .controller('PersonneController', ['$scope', '$http','baseUrl', function ($scope,$http, baseUrl) {
 	  
 	  
