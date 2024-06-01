5f88cfb52de9c5424d842233d3f1785d7eca82fa
Rabeb Nouira <rabebnouira30@gmail.com>
2024-06-01 13:27:24 +0000

    Update config.js

--- ma-gpro-design-war/presentation/src/main/webapp/scripts/config/config.js
+++ ma-gpro-design-war/presentation/src/main/webapp/scripts/config/config.js
@@ -3,7 +3,7 @@
  */
 
 angular.module('config', ['ngResource'])
-    .constant('UrlCommun', "http://localhost:8080/mt-gpro-commun-rest-3.5.0.0-SNAPSHOT")
-    .constant('UrlAtelier', "http://localhost:8080/ma-gpro-logistique-rest-3.5.0.0-SNAPSHOT");
+    .constant('UrlCommun', "http://localhost:8889/mt-gpro-commun")
+    .constant('UrlAtelier', "http://localhost:8890/ma-gpro-logistique");
   // .constant('UrlCommun', 'http://51.83.78.248:8055/mt-gpro-commun-rest')
   // .constant('UrlAtelier', 'http://51.83.78.248:8055/ma-gpro-logistique-rest');
