7cf3cf563e3d1fe1069fe9307e8c2b3cb863dae4
Ubuntu <asmaabdallah518@gmail.com>
2024-06-03 12:26:04 +0000

    updated

--- ma-gpro-design-war/presentation/src/main/webapp/scripts/config/config.js
+++ ma-gpro-design-war/presentation/src/main/webapp/scripts/config/config.js
@@ -3,7 +3,7 @@
  */
 
 angular.module('config', ['ngResource'])
-    .constant('UrlCommun', "http://52.91.134.4:8080/mt-gpro-commun")
-    .constant('UrlAtelier', "http://52.91.134.4:8080/ma-gpro-logistique");
+    .constant('UrlCommun', "http://localhost:8080/mt-gpro-commun")
+    .constant('UrlAtelier', "http://localhost:8080/ma-gpro-logistique");
   // .constant('UrlCommun', 'http://51.83.78.248:8055/mt-gpro-commun-rest')
   // .constant('UrlAtelier', 'http://51.83.78.248:8055/ma-gpro-logistique-rest');
